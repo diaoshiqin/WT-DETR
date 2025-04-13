6290225d0b62adbd28383065c82380ff20a0413f
diaoshiqin <1620402033@qq.com>
2025-04-13 18:15:07 +0800

    Update block.py

--- WT-DETR-main/ultralytics/nn/extra_modules/block.py
+++ WT-DETR-main/ultralytics/nn/extra_modules/block.py
@@ -5,31 +5,26 @@ import numpy as np
 
 
 
-__all__ = ['WaveletUnPool','wtd']
+__all__ = ['WaveletUnPool','Conv']
 
-class wtd(nn.Module):
-    def __init__(self,out_ch):
-        super(wtd, self).__init__()
-        ll = np.array([[0.5, 0.5], [0.5, 0.5]])
-        lh = np.array([[-0.5, -0.5], [0.5, 0.5]])
-        hl = np.array([[-0.5, 0.5], [-0.5, 0.5]])
-        hh = np.array([[0.5, -0.5], [-0.5, 0.5]])
-        filts = np.stack([ll[None,::-1,::-1], lh[None,::-1,::-1],
-                            hl[None,::-1,::-1], hh[None,::-1,::-1]],
-                            axis=0)
-        self.weight = nn.Parameter(
-            torch.tensor(filts).to(torch.get_default_dtype()),
-            requires_grad=False)
-        self.conv_bn_relu = nn.Sequential(
-                                    nn.BatchNorm2d(out_ch),
-                                    nn.ReLU(inplace=True),
-                                    )
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
     def forward(self, x):
-        C = x.shape[1]
-        filters = torch.cat([self.weight,] * C, dim=0)
-        y = F.conv2d(x, filters, groups=C, stride=2)
-        y = self.conv_bn_relu(y)
-        return y
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
 
 
 
